c707d99c808bf65f35cad5835e0a9c4b7640222d
LunarStrain94 <92212138+LunarStrain94@users.noreply.github.com>
2021-11-25 20:19:29 +0100

    Add files via upload

---
+++ Vježba 3 Zad 4-5/Vj3_Zad4/Vj3_Zad4/Header.hpp
@@ -0,0 +1,11 @@
+#ifndef VECTORFUN
+#define VECTORFUN
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void inputVec(vector<int>& v, int n);
+void printVec(vector<int> v);
+void removeVecEl(vector<int>& v, int el);
+
+#endif
