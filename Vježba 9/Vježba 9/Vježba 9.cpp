af8aeb89ef1363f2112f3cf1faa4f016da7230d9
LunarStrain94 <92212138+LunarStrain94@users.noreply.github.com>
2022-02-01 19:03:19 +0100

    Add files via upload

---
+++ Vježba 9/Vježba 9/Vježba 9.cpp
@@ -0,0 +1,28 @@
+﻿#include "Game.hpp"
+
+int main() {
+    srand(time(NULL));
+    int npl, ncpu;
+    HumanPlayer dummy;
+    ComputerPlayer dummy2;
+    vector<HumanPlayer> pl;
+    vector<ComputerPlayer> cpu;
+    
+    cout << "Enter the number of human players: ";
+    cin >> npl;
+    for (int i = 0; i < npl; ++i)
+        pl.push_back(dummy);
+    
+    cout << "Enter the number of computer players: ";
+    cin >> ncpu;
+    for (int i = 0; i < ncpu; ++i)
+        cpu.push_back(dummy2);
+    
+    Game gm(10, npl, ncpu);
+    
+    while (gm.isGameOver(pl, cpu) == false) {
+        gm.round(pl, cpu);
+        gm.scoreboard(pl, cpu);
+    }
+    gm.winner(pl, cpu);
+}
