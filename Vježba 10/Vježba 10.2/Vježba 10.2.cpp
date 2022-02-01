0acd9131cc81a573c8168f951f4ee17c4ff22207
LunarStrain94 <92212138+LunarStrain94@users.noreply.github.com>
2022-02-01 19:04:40 +0100

    Add files via upload

---
+++ Vježba 10/Vježba 10.2/Vježba 10.2.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+template <typename T>
+class Stack {
+public:
+	vector<T> arr;
+	void push(const T& n);
+	void print();
+	void pop();
+	bool isEmpty();
+};
+
+template <typename T>
+void Stack<T>::push(const T& n) {
+	arr.push_back(n);
+}
+
+template <typename T>
+void Stack<T>::pop() {
+	arr.pop_back();
+}
+
+template <typename T>
+void Stack<T>::print() {
+	int len = arr.size();
+	for (int i = 0; i < len; ++i)
+		cout << arr[i] << " ";
+}
+
+template <typename T>
+bool Stack<T>::isEmpty() {
+	return arr.size();
+}
+
+/*template <>
+void Stack<char>::print() {
+	int len = arr.length();
+	for (int i = 0; i < len; ++i)
+		cout << arr[i];
+}*/
+
+void main() {
+	Stack<int> arr;
+	arr.push(1);
+	arr.push(2);
+	arr.push(3);
+	arr.push(4);
+	arr.pop();
+	arr.print();
+}
---
+++ Vježba 10/Vježba 10/Vježba 10.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+template <typename T>
+void sortArr(T arr[], int n) {
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) {
+			if (arr[i] < arr[j])
+				swap(arr[i], arr[j]);
+		}
+	}
+}
+
+template <>
+void sortArr<char>(char arr[], int n) {
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) {
+			if (tolower(arr[i]) < tolower(arr[j]))
+				swap(arr[i], arr[j]);
+		}
+	}
+}
+
+int main() {
+    int a1[10] = {5, 4, 1, 6, 9, 8, 7, 2, 3, 2};
+    char s[15] = {'T', 'h', 'y', ' ', 'C', 'a', 't', 'a', 'f', 'a', 'l', 'q', 'u', 'e'};
+    for (int i = 0; i < 10; ++i)
+        cout << a1[i] << " ";
+    cout << s << endl;
+
+    sortArr(a1, 10);
+    sortArr(s, 14);
+    for (int i = 0; i < 10; ++i)
+        cout << a1[i] << " ";
+	cout << s << endl;
+}
